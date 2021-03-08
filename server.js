6638abbb216ae5ceccb3108c24eac67c8b17e069
Ankit Saxena <ankit.saxena@deeplogictech@gmail.com>
2021-03-08 16:30:43 +0530

    Get Todos created

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express   = require('express');
+const app       = express();
+const PORT      = process.env.PORT || 3000;
+
+let todos = [{
+    id:1,
+    description:'Breakfast with Modi',
+    completed:false
+},{
+    id:2,
+    description:'Go to the Market in Evening',
+    completed:false
+}];
+
+
+app.get('/todos',function(){
+    res.json(todos);
+});
+
 
 app.get('/', function(req,res){
     res.send('Todo API Root');
