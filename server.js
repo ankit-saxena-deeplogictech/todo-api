51980b6f777cb509aa0a553fa71de40184cd9bd1
Ankit Saxena <ankit.saxena@deeplogictech@gmail.com>
2021-03-09 12:56:46 +0530

    adding Delete Todo request

--- server.js
+++ server.js
@@ -50,6 +50,19 @@ app.post('/todos',function(req,res){
     res.json(body);
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id',function(req,res){
+    const todoId = parseInt(req.params.id);
+    let matchedTodo = _.findWhere(todos,{id:todoId})
+    if(!matchedTodo){
+        res.status(404).json({"error":"No todo found with id : "+todoId});
+    } else {
+        todos   =   _.without(todos,matchedTodo);
+        res.json(matchedTodo);
+    }
+
+});
+
 app.listen(PORT, function(){
     console.log('Express listening on : '+PORT);
 });
